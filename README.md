11e4948dc52985e37e6f3cb33da6d7e4e928b1d9
unknown <chahaldarren@gmail.com>
2024-06-27 14:58:33 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+<!-- This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
 
@@ -33,4 +33,65 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details. -->
+
+
+
+
+# Price Watch Dog
+
+Price Watch Dog is a web application built with Next.js that allows users to input the URL of an Amazon product. The app scrapes Amazon to show the current highest, lowest, and average prices of the product. Additionally, users can opt to track the product's price, and the app will notify them when the price drops.
+
+## Features
+
+- **Product Price Scraping**: Input an Amazon product URL to see the current highest, lowest, and average prices.
+- **Price Tracking**: Track the price of a product and receive notifications when the price drops.
+- **Cron Jobs**: Automated price checking at regular intervals using cron jobs.
+
+## Tech Stack
+
+- **Frontend**: Next.js, Next.js, Headless UI, Tailwind CSS
+- **Web Scraping**: Cheerio, Bright Data
+- **Cron Jobs**: Node-cron
+- **Notifications**: Nodemailer (for email notifications)
+- **Database**: MongoDB
+
+## Installation
+
+1. **Clone the repository:**
+   ```sh
+   git clone https://github.com/your-username/price-watch-dog.git
+   cd price-watch-dog
+2. **Install dependencies:**
+    ```sh
+    npm install
+3. **Set up environment variables:**
+    Create a `.env` file in the root directory and add the following variables:
+
+    ### Scraper
+    BRIGHT_DATA_USERNAME= \
+    BRIGHT_DATA_PASSWORD=
+
+    ### Database
+    MONGODB_URI=
+
+    ### Email
+    EMAIL_PASSWORD=
+
+4. **Run the deployment server:**
+    ```sh
+    npm run dev
+Open http://localhost:3000 with your browser to see the result.
+
+## Usage
+
+1. **Enter the Amazon product URL:**
+   On the home page, paste the URL of the Amazon product you want to track.
+
+2. **View Price Information:**
+   After submitting the URL, you will see the current highest, lowest, and average prices of the product.
+
+3. **Track the Product:**
+   Click on the "Track" button to start tracking the product's price. You will be notified via email when the price drops.
+
+
---
+++ lib/scraper/index.ts
@@ -0,0 +1,92 @@
+import axios from "axios";
+import * as cheerio from "cheerio";
+import { extractCurrency, extractDescription, extractPrice } from "../utils";
+
+export async function scrapeAmazonProduct(url: string){
+    if(!url){
+        return
+    }
+
+    //curl --proxy brd.superproxy.io:22225 --proxy-user brd-customer-hl_8116e6a7-zone-web_unlocker1:fkvd18ei72ih -k "http://geo.brdtest.com/mygeo.json"
+    const username = String(process.env.BRIGHT_DATA_USERNAME);
+    const password = String(process.env.BRIGHT_DATA_PASSWORD);
+    const port = 22225;
+
+    const session_id = (1000000*Math.random()) | 0;
+    const options = {
+        auth:{
+            username: `${username}-session-${session_id}`,
+            password: password,
+        },
+        host: 'brd.superproxy.io',
+        port,
+        rejectUnauhtorized: false,
+
+    }
+
+    try{
+        const response = await axios.get(url, options);
+        const $ = cheerio.load(response.data);
+        const title = $('#productTitle').text().trim();
+        const currentPrice  = extractPrice(
+            $('.priceToPay span.a-price-whole'),
+            $('.a.size.base.a-color-price'),
+            $('.a-button-selected .a-color-base'),
+            
+        );
+        const originalPrice = extractPrice(
+            $('#priceblock_ourprice'),
+            $('.a-price.a-text-price span.a-offscreen'),
+            $('#listPrice'),
+            $('#priceblock_dealprice'),
+            $('.a-size-base.a-color-price')
+        )
+        const outOfStock = $('#availability span').text().trim().toLowerCase() === 'currently unavailable';
+
+        const images = 
+            $('#imgBlkFront').attr('data-a-dynamic-image') || 
+            $('#landingImage').attr('data-a-dynamic-image') ||
+            '{}';
+
+        const imageUrls = Object.keys(JSON.parse(images));
+
+        const currency = extractCurrency($('.a-price-symbol'))
+        const discountRate = $('.savingsPercentage').text().replace(/[-%]/g, "");
+        const description = extractDescription($)
+        const starsText = $('#acrPopover > span.a-declarative > a > span').text().trim();
+        const starsMatch = starsText.match(/\d+(\.\d+)?/); 
+        const stars = starsMatch ? starsMatch[0] : ''; 
+
+        const reviewsCountText = $('#acrCustomerReviewText').text().trim();
+        const reviewsCountMatch = reviewsCountText.match(/\d+/); 
+        const reviewsCount = reviewsCountMatch ? parseInt(reviewsCountMatch[0]) : 0;
+        //const category = $('#nav-subnav > a.nav-a.nav-hasImage > span').text().trim();
+        // console.log({category})
+        
+        const data = {
+            url,
+            currency: currency || '$',
+            image: imageUrls[0],
+            title,
+            currentPrice: Number(currentPrice) || Number(originalPrice),
+            originalPrice: Number(originalPrice) || Number(currentPrice),
+            priceHistory: [],
+            discountRate: Number(discountRate),
+            category: 'category',
+            reviewsCount: Number(reviewsCount),
+            stars: Number(stars),
+            isOutOfStock: outOfStock,
+            description,
+            lowestPrice: Number(currentPrice) || Number(originalPrice),
+            highestPrice: Number(originalPrice) || Number(currentPrice),
+            averagePrice: Number(currentPrice) || Number(originalPrice),
+        }
+
+        return data;
+
+    }catch(error:any){
+        throw new Error(`Failed to scrape the Product: ${error.message}`)
+
+    }
+
+}
